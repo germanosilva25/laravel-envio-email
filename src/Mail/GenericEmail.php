b4e2ab65ed675bd93d4bd144211befe1e26d7566
germanosilva25 <42486346+germanosilva25@users.noreply.github.com>
2025-09-22 13:38:12 -0300

    correção e-mail corpo

--- src/Mail/GenericEmail.php
+++ src/Mail/GenericEmail.php
@@ -6,26 +6,27 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class GenericEmail extends Mailable implements ShouldQueue
+
+class GenericEmail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    public $subjectLine;
     public $bodyContent;
+    public $subjectLine;
 
-    public function __construct($subjectLine, $bodyContent)
+    public function __construct($subject, $body)
     {
-        $this->subjectLine = $subjectLine;
-        $this->bodyContent = $bodyContent;
+        $this->subjectLine = $subject;
+        $this->bodyContent = $body;
+        $this->subject($subject); // define o subject
     }
 
     public function build()
     {
-        return $this->subject($this->subjectLine)
-                    ->view('envio-email::emails.generic')
-                    ->with([
-                        'bodyContent' => $this->bodyContent,
-                        'subjectLine' => $this->subjectLine,
-                    ]);
+        return $this
+            ->subject($this->subjectLine)
+            ->view('emails.generic') // ✅ view HTML
+            ->with([
+                'bodyContent' => $this->bodyContent,
+                'subjectLine' => $this->subjectLine,
+            ]);
     }
 }
